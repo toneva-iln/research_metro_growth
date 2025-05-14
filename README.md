de9e333ee2e43c7691f5e2d637a2ad93661de984
Iliana Toneva <tonevailiana@gmail.com>
2025-05-13 23:39:07 -0700

    error handling

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # research_metro_growth
 Micro SaaS For Searching and Sorting Metro Growth Prediction Data
+
+### Prerequisites
+
+1. Install JSON Viewer Pro Chrome extension
+
+2. 
--- main.py
+++ main.py
@@ -55,23 +55,36 @@ def search(
     second_last: str = Query(None, description="Value in the second to last column"),
     third_last: str = Query(None, description="Value in the third to last column"),
 ):
-    df = load_data()
-    df = df.replace({np.nan: None})
+    try:
+        df = load_data()
+        df = df.replace({np.nan: None})
 
-    last_col = df.columns[-1]
-    second_last_col = df.columns[-2]
-    third_last_col = df.columns[-3]
+        last_col = df.columns[-1]
+        second_last_col = df.columns[-2]
+        third_last_col = df.columns[-3]
 
-    if state:
-        df = df[df["StateName"].str.lower() == state.lower()]
+        if state:
+            df = df[df["StateName"].str.lower() == state.lower()]
 
-    for search_term, col in [
-        (last, last_col),
-        (second_last, second_last_col),
-        (third_last, third_last_col),
-    ]:
-        if search_term:
-            op, val = parse_filter_expr(search_term)
-            df = df[df[col].apply(lambda x: op(x, val) if pd.notnull(x) else False)]
+        for search_term, col in [
+            (last, last_col),
+            (second_last, second_last_col),
+            (third_last, third_last_col),
+        ]:
+            if search_term:
+                op, val = parse_filter_expr(search_term)
+                df = df[df[col].apply(lambda x: op(x, val) if pd.notnull(x) else False)]
+    except KeyError:
+        err_msg = "No results found for the following parameters: "
+        if state:
+            err_msg += f"State  {state} "
+        if third_last:
+            err_msg += f"Near Future Prediction value: {third_last}"
+        if second_last:
+            err_msg += f"Next 6 Months Prediction value: {second_last}"
+        if last:
+            err_msg += f"Next 9-12 Months Prediction value: {last}"
+
+        return JSONResponse(status_code=400, content={"error": err_msg})
 
     return JSONResponse(content=df.to_dict(orient="records"))
